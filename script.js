01d70c74d1dad9e93b29575d8b7a8e71f936e012
nimratahir142 <khanzaadi142@gmail.com>
2024-12-25 15:50:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let x = 15;
+let y = 12;
+let z = x+y;
+document.getElementById('one').innerHTML = +z;
+let a = 5;
+let b = 2;
+let c = a+b;
+document.getElementById('two').innerHTML = +c;
+let l = 6;
+let m = 6;
+let n = l*m;
+document.getElementById('three').innerHTML = n;
+let p = 2;
+let r = (25 + 20) *p;
+
+
